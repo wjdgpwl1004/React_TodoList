d93859469fb41e208ac86a619ca6595c0e6f2cd9
정곰 <wjdgpwl1004@naver.com>
2020-05-24 23:54:58 +0900

    first commit

---
+++ src/ex/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+
+const App = () => {
+
+  return (
+   <>
+   <div className="ss">
+sssss
+   </div>
+    <div className="box">
+        <div className="contbox">
+            <div className="cont">
+
+            </div>
+        </div>
+    </div>
+   </>
+  );
+
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
